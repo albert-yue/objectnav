ae92668348f7abe25d6ffb5ebdfd3ed0b7677697
Albert Yue <ayue@mit.edu>
2022-02-10 13:45:38 -0500

    Deduplicate MP3D category mapping

--- habitat/tasks/nav/object_nav_task.py
+++ habitat/tasks/nav/object_nav_task.py
@@ -18,6 +18,7 @@ from habitat.core.logging import logger
 from habitat.core.registry import registry
 from habitat.core.simulator import AgentState, Sensor, SensorTypes
 from habitat.core.utils import not_none_validator
+from habitat.tasks.nav.consts import task_cat2mpcat40
 from habitat.tasks.nav.nav import (
     NavigationEpisode,
     NavigationGoal,
@@ -27,36 +28,6 @@ from habitat.tasks.nav.nav import (
     TopDownMap,
 )
 
-task_cat2mpcat40 = [
-    3,  # ('chair', 2, 0)
-    5,  # ('table', 4, 1)
-    6,  # ('picture', 5, 2)
-    7,  # ('cabinet', 6, 3)
-    8,  # ('cushion', 7, 4)
-    10,  # ('sofa', 9, 5),
-    11,  # ('bed', 10, 6)
-    13,  # ('chest_of_drawers', 12, 7),
-    14,  # ('plant', 13, 8)
-    15,  # ('sink', 14, 9)
-    18,  # ('toilet', 17, 10),
-    19,  # ('stool', 18, 11),
-    20,  # ('towel', 19, 12)
-    22,  # ('tv_monitor', 21, 13)
-    23,  # ('shower', 22, 14)
-    25,  # ('bathtub', 24, 15)
-    26,  # ('counter', 25, 16),
-    27,  # ('fireplace', 26, 17),
-    33,  # ('gym_equipment', 32, 18),
-    34,  # ('seating', 33, 19),
-    38,  # ('clothes', 37, 20),
-    43,  # ('foodstuff', 42, 21),
-    44,  # ('stationery', 43, 22),
-    45,  # ('fruit', 44, 23),
-    46,  # ('plaything', 45, 24),
-    47,  # ('hand_tool', 46, 25),
-    48,  # ('game_equipment', 47, 26),
-    49,  # ('kitchenware', 48, 27)
-]
 
 @attr.s(auto_attribs=True, kw_only=True)
 class ObjectGoalNavEpisode(NavigationEpisode):
