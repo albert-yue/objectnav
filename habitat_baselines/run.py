2f66a875ea359cd5a3c19764062e26a732cfa4ba
Albert Yue <ayue@mit.edu>
2022-02-10 13:53:10 -0500

    Add visual observations to save during eval

--- habitat_baselines/run.py
+++ habitat_baselines/run.py
@@ -256,6 +256,7 @@ def run_exp(
             # Also record probing information
             log_diagnostics = [
                 Diagnostics.basic, Diagnostics.internal_activations, Diagnostics.observations,
+                Diagnostics.visual_observations,
                 Diagnostics.actions, Diagnostics.episode_info, Diagnostics.weights,
                 Diagnostics.d2g, Diagnostics.room_cat, Diagnostics.visit_count,
                 Diagnostics.collisions_t, Diagnostics.coverage_t, Diagnostics.sge_t
